4496267e7d851a4a5088053d669a6231ff17ff28
Amit Kanti Barua <amitkanti.barua@petronas.com>
2024-05-23 14:00:12 +0600

    First commit on 23/05/2024

---
+++ DragControlsDemo/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DragControlsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MyPanel = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(656, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MyPanel
+            // 
+            this.MyPanel.BackColor = System.Drawing.Color.White;
+            this.MyPanel.Location = new System.Drawing.Point(1, 0);
+            this.MyPanel.Name = "MyPanel";
+            this.MyPanel.Size = new System.Drawing.Size(627, 404);
+            this.MyPanel.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(660, 158);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(872, 401);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyPanel);
+            this.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customizable Drag-and-Drop UI Form Builder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel MyPanel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DragControlsDemo/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragControlsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<TextBox> aTextLst = new List<TextBox>();
+        List<Label> aLabelLst = new List<Label>();
+        List<Button> aButtonLst = new List<Button>();
+
+        int countTextBox = 0;
+        int countLabel = 0;
+        int countButton = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ControlExtension.Draggable(button1, true);
+
+            foreach (TextBox item in aTextLst)
+            {
+                ControlExtension.Draggable(item, true);
+            }
+
+            foreach (Label item in aLabelLst)
+            {
+                ControlExtension.Draggable(item, true);
+            }
+
+            foreach (Button item in aButtonLst)
+            {
+                ControlExtension.Draggable(item, true);
+            }
+
+            ControlExtension.Draggable(MyPanel, false);
+            ControlExtension.Draggable(textBox1, false);
+            ControlExtension.Draggable(button1, false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            countButton++;
+
+            Button aButton = new Button();
+            aButton.Name = "Button" + countTextBox;
+            aButton.Text = "Your Custom Button";
+            aButton.Font = new Font("Arial", 6, FontStyle.Bold);
+            aButton.AutoSize = true;
+
+            aButtonLst.Add(aButton);
+
+            foreach (Button item in aButtonLst)
+            {
+                MyPanel.Controls.Add(item);
+                ControlExtension.Draggable(item, true);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            countTextBox++;
+
+            TextBox aTextBox = new TextBox();
+            aTextBox.Name = "Text" + countTextBox;
+            aTextBox.Text = "Your Custom TextBox";
+            aTextBox.Font = new Font("Arial", 6, FontStyle.Bold);
+            aTextBox.AutoSize = true;
+            aTextBox.Multiline = true; 
+
+            aTextLst.Add(aTextBox);
+
+            foreach (TextBox item in aTextLst)
+            {
+                MyPanel.Controls.Add(item);
+                ControlExtension.Draggable(item, true);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            countLabel++;
+
+            Label aLabel = new Label();
+            aLabel.Name = "Text" + countLabel;
+            aLabel.Text = "Your Custom Label";
+            aLabel.Font = new Font("Arial", 6, FontStyle.Bold);
+
+            aLabelLst.Add(aLabel);
+
+            foreach (Label item in aLabelLst)
+            {
+                MyPanel.Controls.Add(item);
+                ControlExtension.Draggable(item, true);
+            }
+        }
+    }
+}
